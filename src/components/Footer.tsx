413ba1f5e162e79c51c4e5ffc100881db5e1b68c
Abdullahi Shobaloju <sabdullahi@cinnsol.com>
2024-10-10 15:31:46 +0100

    google font setup

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Footer () {
+    return (
+        <div className="text-white">This is the footer component.</div>
+    )
+}
+
--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-
-
-const Header = () => {
-    return (
-        <div className="text-white">This is the header component</div>
-    )
-}
-
-export default Header
