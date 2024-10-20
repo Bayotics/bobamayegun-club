5c955b41a6348a8a5ad2623aa934fde6ad91eb88
Abdullahi Shobaloju <sabdullahi@cinnsol.com>
2024-10-20 09:57:48 +0100

    GALLERY  SECTION INITIATED

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { AboutUs } from "@/components/HomePage/AboutUs";
 import { Initiatives } from "../components/HomePage/Initiatives";
 import { WhyUs } from "@/components/HomePage/WhyUs";
 import OurExcos from "@/components/HomePage/Excos/OurExcos";
+import Gallery from "@/components/HomePage/Gallery";
 
 export default function Home() {
   return (
@@ -24,8 +25,11 @@ export default function Home() {
       <div className="home-why-us">
         <WhyUs />
       </div>
-      <div className="home-our-excos pt-24 bg-gray-200 mb-44">
+      <div className="home-our-excos pt-24 bg-gray-200">
         <OurExcos />
+      </div>
+      <div className="home-gallery mt-20 mb-44">
+        <Gallery />
       </div>
     </div>
   );
